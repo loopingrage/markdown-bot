649af4c82b742383b27968b381d1ac77ec224c48
Jose de Castro <loopingrage@gmail.com>
2016-07-03 15:01:09 -0700

    Added some logging

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(bodyParser.json());
 app.post('/', function (req, res) {
   console.log("New message:", util.inspect(req.body.data));
   spark.messages.get(req.body.data).then(function(message) {
-    spark.messages.create({
-      roomId: message.roomId,
-      markdown: message.text
-    });
+    if(message.markdown == null) {
+      spark.messages.create({
+        roomId: message.roomId,
+        markdown: message.text
+      });
+    }
     res.send(204);
   }).catch(function(err) {
     console.err('Error getting message text');
