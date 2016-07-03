960c3e017320994c5e6ff305937df228bf52fbea
Jose de Castro <loopingrage@gmail.com>
2016-07-03 14:22:42 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+var spark = require('ciscospark');
+var express = require('express');
+var app = express();
+
+app.post('/', function (req, res) {
+  spark.messages.create({
+    roomId: req.body.roomId,
+    markdown: req.body.text
+  });
+  req.send(204);
+});
+
+app.listen(3000 || env.PORT, function () {
+  console.log('Listening on port 3000!');
+});
