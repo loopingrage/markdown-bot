f6ec6a4b4d1b969c30f3d835de03797d00a8359a
Jose de Castro <loopingrage@gmail.com>
2016-07-03 14:57:02 -0700

    Added some logging

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  console.log("New message:", util.inspect(req.body));
-  spark.messages.create({
-    roomId: req.body.data.roomId,
-    markdown: req.body.data.text
+  console.log("New message:", util.inspect(req.body.data));
+  spark.messages.get(req.body.data).then(function(message) {
+    spark.messages.create({
+      roomId: message.roomId,
+      markdown: message.text
+    });
+    res.send(204);
+  }).catch(function(err) {
+    console.err('Error getting message text');
+    res.send(500);
   });
-  res.send(204);
 });
 
 app.listen(process.env.PORT || 3000, function () {
