cb977448f2785b0c9f8f00e9d889cb767ca5c1c0
Jose de Castro <loopingrage@gmail.com>
2016-07-03 15:09:44 -0700

    Added some logging

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ var util = require('util');
 var app = express();
 app.use(bodyParser.json());
 
+var ACTIVATION_CHAR = '!';
+
 app.post('/', function (req, res) {
   console.log("New message:", util.inspect(req.body.data));
   spark.messages.get(req.body.data.id).then(function(message) {
-    if(message.markdown == null) {
+    if(message.markdown == null && message.text.indexOf(ACTIVATION_CHAR) == 0) {
       spark.messages.create({
         roomId: message.roomId,
         markdown: message.text
